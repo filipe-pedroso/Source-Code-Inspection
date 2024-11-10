bb9d872957a3869211f1adb021115690f8f40dcf
Daniel Alves Cunha <86078001+DanielAC12@users.noreply.github.com>
2024-11-09 23:31:44 -0300

    Test 18

--- src/test/java/com/exemplo/AppTest.java
+++ src/test/java/com/exemplo/AppTest.java
@@ -2,25 +2,43 @@ package com.exemplo;
 
 import org.junit.Assert;
 import org.junit.Test;
-import br.calebe.ticketmachine.core.TicketMachine;
-import br.calebe.ticketmachine.exception.SaldoInsuficienteException;
-import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
+import br.calebe.ticketmachine.core.PapelMoeda; 
 
+/**
+ * Test class for PapelMoeda.
+ */
 public class AppTest {
 
-    @Test(expected = SaldoInsuficienteException.class)
-    public void testEmitirTicketComSaldoExato() throws SaldoInsuficienteException, PapelMoedaInvalidaException {
-        TicketMachine machine = new TicketMachine(17);
-        machine.inserir(7);
-        machine.inserir(10);
-        Assert.assertEquals(17, machine.getSaldo());
+    @Test
+    public void testGetValor() {
+        PapelMoeda papelMoeda = new PapelMoeda(15, 7);
+        Assert.assertEquals("O valor do papel-moeda deve ser 15.", 15, papelMoeda.getValor());
     }
 
-    @Test(expected = SaldoInsuficienteException.class)
-    public void testInserirDinheiroMultiplasVezes() throws PapelMoedaInvalidaException {
-        TicketMachine machine = new TicketMachine(15);
-        machine.inserir(5);
-        machine.inserir(10);
-        Assert.assertEquals(15, machine.getSaldo());
+    @Test
+    public void testGetQuantidade() {
+        PapelMoeda papelMoeda = new PapelMoeda(15, 7);
+        Assert.assertEquals("A quantidade de papel-moeda deve ser 7.", 7, papelMoeda.getQuantidade());
+    }
+
+    @Test
+    public void testCriarPapelMoedaComValoresPositivos() {
+        PapelMoeda papelMoeda = new PapelMoeda(100, 4);
+        Assert.assertEquals("O valor do papel-moeda deve ser 100.", 100, papelMoeda.getValor());
+        Assert.assertEquals("A quantidade de papel-moeda deve ser 4.", 4, papelMoeda.getQuantidade());
+    }
+
+    @Test
+    public void testCriarPapelMoedaComQuantidadeZero() {
+        PapelMoeda papelMoeda = new PapelMoeda(50, 0);
+        Assert.assertEquals("O valor do papel-moeda deve ser 50.", 50, papelMoeda.getValor());
+        Assert.assertEquals("A quantidade de papel-moeda deve ser 0.", 0, papelMoeda.getQuantidade());
+    }
+    
+    @Test
+    public void testCriarPapelMoedaComQuantidadeNegativa() {
+        PapelMoeda papelMoeda = new PapelMoeda(30, -5);
+        Assert.assertEquals("O valor do papel-moeda deve ser 30.", 30, papelMoeda.getValor());
+        Assert.assertEquals("A quantidade de papel-moeda deve ser -5.", -5, papelMoeda.getQuantidade());
     }
 }
